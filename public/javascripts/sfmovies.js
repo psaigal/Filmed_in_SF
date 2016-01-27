c532ea77c8921b3ff6deb3cda87308957e901643
Priyanka Saigal <email": null>
2016-01-27 15:16:31 -0800

    Extract movie name and location from SF film data. Put data into new object

--- public/javascripts/sfmovies.js
+++ public/javascripts/sfmovies.js
@@ -1,8 +1,17 @@
 $.getJSON('https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD', function(data) {
     //data is the JSON string
-   console.log(data);
+    var arrayOfObjects = []
+    var arrayOfInformation = data.data;
+      for (index in arrayOfInformation) {
+        arrayOfObjects.push({name:arrayOfInformation[index][14], location: arrayOfInformation[index][10]})
+    }
+    // input = "Luis Mandoki";
+    for (index in arrayOfObjects) {
+      if (arrayOfObjects[index].name == input) {
+        $("#container").append(arrayOfObjects[index].location + "<br>");
+      }
+    }
 });
-
 
 
 
