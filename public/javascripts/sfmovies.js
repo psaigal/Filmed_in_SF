8a85be75c0120d8ad260c912937834dad7d5389a
Priyanka Saigal <email": null>
2016-01-27 18:20:14 -0800

    When movie name is put into search box, movie location is displayed on home page

--- public/javascripts/sfmovies.js
+++ public/javascripts/sfmovies.js
@@ -1,20 +1,16 @@
 $(document).ready(function() {
 
+  arrayOfObjects = []
+
   $.getJSON('https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD', function(data) {
       //data is the JSON string
-      var arrayOfObjects = []
       var arrayOfInformation = data.data;
         for (index in arrayOfInformation) {
           arrayOfObjects.push({name:arrayOfInformation[index][14], location: arrayOfInformation[index][10]})
       }
-      input = "Luis Mandoki";
-      for (index in arrayOfObjects) {
-        if (arrayOfObjects[index].name == input) {
-          $("#container").append(arrayOfObjects[index].location + "<br>");
-        }
-      }
 
-      $('#movie-form').submit(function(event) {
+
+    $('#movie-form').submit(function(event) {
     event.preventDefault();
     var path = $('#movie-form').attr("action")
     var formData = $('#movie-form').serialize();
@@ -33,20 +29,14 @@ $(document).ready(function() {
     }
     })
     .done(function(response){
-      movieSearchTerm = response.nameOfMovie);
-
+      movieSearchTerm = response.nameOfMovie;
+      for (index in arrayOfObjects) {
+        if (arrayOfObjects[index].name == movieSearchTerm) {
+          $("#container").append(arrayOfObjects[index].location + "<br>");
+        }
+      }
     });
   });
-
-
-
-
-
   });
-
-
-
-
-
 });
 
